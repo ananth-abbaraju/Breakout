c8aaf7e2623a8c54401f242093e412fb491f3419
Ananth Abbaraju <36719875+ananth-abbaraju@users.noreply.github.com>
2022-06-19 18:23:19 -0500

    Update Brick.java

--- src/java/Brick.java
+++ src/java/Brick.java
@@ -3,14 +3,11 @@ import java.awt.Rectangle;
 import java.awt.Color;
 
 public class Brick {
-    // Declartion of instance variables/objects
 
     private Breakout breakout;
     private int xPos, yPos;
     private int width, height;
-    // constructor is first thing called when a new Paddle is created
-    // the constructor is where any instance variables are given values
-    // initializes instance variables
+
     public Brick(int x, int y, Breakout out) {
         xPos = x;
         yPos = y;
@@ -20,8 +17,8 @@ public class Brick {
         breakout = out;
     }
 
-
     public void paint(Graphics g) {
+        
         if (yPos == 3){
             g.setColor(new Color(0, 255, 255));
         } else if (yPos == 28) {
@@ -29,31 +26,19 @@ public class Brick {
         } else if (yPos == 53){
             g.setColor(new Color(255, 20, 147));
         }
+        
         g.fillRect(xPos, yPos, width, height);
         g.setColor(Color.BLACK);
         g.drawRect(xPos, yPos, width, height);
+        
     }
 
-    /* -------------------- How to check collision ---------------------- */
-    /**
-     * To have some type of collusion, the brick needs to have bounds In the class
-     * Rectangle, there is a getBounds method
-     *
-     * @return rectangle's outer most values
-     */
     public Rectangle getBounds() {
         return new Rectangle(xPos, yPos, width, height);
     }
 
-    /**
-     * There also needs to be collision between the brick and the ball the following
-     * method will take care of the collision
-     * 
-     * @return true if the ball intersects the brick
-     */
     public boolean collision() {
         return breakout.getBall().getBounds().intersects(getBounds());
     }
 
-    /* ------------------- Getters and Setters -------------------- */
 }
