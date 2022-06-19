d6e7b0ca10480d118edd59c1553d561b51aa711e
Ananth Abbaraju <36719875+ananth-abbaraju@users.noreply.github.com>
2022-06-19 18:26:54 -0500

    Update Score.java

--- src/java/Score.java
+++ src/java/Score.java
@@ -5,25 +5,25 @@ import javax.swing.JPanel;
 import java.awt.font.*;
 
 public class Score extends JPanel {
+    
     private Breakout breakout;
     private int width, height;
+    
     public Score(Breakout panel) {
         width = 160;
         height = 35;
         setSize(width, height);
-        setLocation(30, 25); // x = 200, y = 50
+        setLocation(30, 25); 
         setLayout(null);
         setBackground(Color.BLACK);
         setBorder(BorderFactory.createLineBorder(Color.MAGENTA, 3));
         breakout = panel;
     }
 
-
     public void paint(Graphics g){
         super.paint(g);
         g.setFont(new java.awt.Font("Arial", 2, 24));
         g.setColor(Color.MAGENTA);
         g.drawString("SCORE: " + breakout.getScore(), 5, 30);
-
     }
 }
