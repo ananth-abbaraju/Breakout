f2b57b4ba5ff08fdab6ab626f2e2b74640b2b0fb
Ananth Abbaraju <36719875+ananth-abbaraju@users.noreply.github.com>
2022-06-19 18:23:40 -0500

    Update Game.java

--- src/java/Game.java
+++ src/java/Game.java
@@ -1,7 +1,8 @@
 public class Game {
+    
     public static void main(String[] args) throws InterruptedException {
-        // create an instance of the GameWindow class
-        // window is the object
         GameWindow window = new GameWindow("Breakout - Abbaraju", 800, 600);
+        
     }
+    
 }
